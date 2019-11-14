f4aa3ee7bfd1afc337c4602f21e44d4e6484f16d
PatrykWojciechowski <patrickwww94@gmail.com>
2019-11-14 20:04:05 +0100

    Initial work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+class Hero{
+  constructor(name: string, title: string, imageUrl: string) {
+    this.name = name;
+    this.title = title;
+    this.imageUrl = imageUrl;
+  }
+  name: string;
+  title: string;
+  imageUrl: string;
+}
+
+let HEROES: Hero[] = [
+  new Hero("Patryk", "", ""),
+  new Hero("Piotr", "", ""),
+  new Hero("Przemek", "", "")
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +24,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'checks-app';
+  heroes = HEROES;
+
+  chooseHero() {
+    console.log("Hero chosen!")
+  }
 }
