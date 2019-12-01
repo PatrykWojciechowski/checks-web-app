9345193cd29bf461534127086a7f838c5531fcf2
PatrykWojciechowski <patrickwww94@gmail.com>
2019-12-01 16:56:24 +0100

    Add new modules

---
+++ src/app/display-expenses/display-expenses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-expenses',
+  templateUrl: './display-expenses.component.html',
+  styleUrls: ['./display-expenses.component.scss']
+})
+export class DisplayExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/display-expenses/display-expenses.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DisplayExpensesComponent } from './display-expenses.component';
+
+@NgModule({
+  declarations: [DisplayExpensesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DisplayExpensesModule { }
