463c3f86cbffec3bb32f14cc99654ae6af494782
PatrykWojciechowski <patrickwww94@gmail.com>
2020-02-19 15:48:48 +0100

    Export firebase from environment variables to separate file

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {RouterModule} from "@angular/router";
 import {AngularFireModule} from "@angular/fire";
-import {environment} from "../../environments/environment";
 import {AngularFireDatabaseModule} from "@angular/fire/database";
 import {SharedModule} from "../shared/shared.module";
 import {LoginComponent} from './login/login.component';
 import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {NavigationComponent} from './navigation/navigation.component';
+import {firebaseConfig} from '../../environments/firebase-config';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import {NavigationComponent} from './navigation/navigation.component';
     BrowserAnimationsModule,
     RouterModule,
     SharedModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFirestoreModule,
     AngularFireAuthModule,
